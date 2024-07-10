589aedd7daeb10af7488ae0e8eb0d73a8c6a07f6
Victor <victorrodrigues288@gmail.com>
2024-07-10 16:22:16 -0300

    Rotas produto criado

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const produtos = [];
+
+// Retorno dos Produtos
+server.get('/produtos', (req, res) =>{
+    return res.json(produtos)
+});
+
+// Criar um novo Produto
+server.post('/criarProduto', (req, res) => {
+    const { name, descricao } = req.body;
+    const novoProduto = { name, descricao };
+    produtos.push(novoProduto);
+
+    return res.json(produtos)
+});
+
+// Excluir Produto
+server.delete('/excluirProduto/:index', (req, res) => {
+    const { index } = req.params;
+
+    if (index >= 0 && index < produtos.length) {
+        produtos.splice(index, 1); // Remove o elemento do array no índice especificado
+        return res.json(produtos);
+    } else {
+        return res.status(404).json({ error: 'Produto não encontrado' });
+    }
+});
+
+
+
+
+server.listen(1500);
