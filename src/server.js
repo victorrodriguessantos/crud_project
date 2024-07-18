77d19158a64116c41c83e62f3ce7b271bc3648f3
Victor <victorrodrigues288@gmail.com>
2024-07-18 00:27:09 -0300

    Ajustes nas pastas

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const { path } = require('express/lib/application');
+
+require('dotenv').config({path:'variables.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser'); // modulo que converte o body da requisição para outros formatos
+
+const routes = require('./routes');
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded({extended:false}));
+
+server.listen(process.env.PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+})
